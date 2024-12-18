28b0f8528767b496d95a15cdb0eb5c7d497d45b4
sujitha.raje <sujitha.raje@zohocorp.com>
2024-12-18 18:32:34 +0530

    second

--- cases/Test.cpp
+++ cases/Test.cpp
@@ -1,7 +1,65 @@
-#include <cstdlib>
-#include <cstdio>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char **argv)
-{
-    system(argv[1]);
+// A simple implementation of encodeShellString
+// Escapes special characters for shell commands.
+void encodeShellString(char *dest, size_t destSize, const char *src) {
+    size_t destIndex = 0;
+    for (size_t i = 0; src[i] != '\0' && destIndex < destSize - 1; i++) {
+        switch (src[i]) {
+            case ' ':
+            case '"':
+            case '\'':
+            case '\\':
+            case '&':
+            case ';':
+            case '|':
+            case '<':
+            case '>':
+            case '`':
+            case '$':
+                if (destIndex + 2 >= destSize - 1) {
+                    break; // Ensure space for the escape and the null terminator
+                }
+                dest[destIndex++] = '\\';
+                dest[destIndex++] = src[i];
+                break;
+            default:
+                dest[destIndex++] = src[i];
+                break;
+        }
+    }
+    dest[destIndex] = '\0';
 }
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <unused_arg> <user_name>\n", argv[0]);
+        return 1;
+    }
+
+    char *userName = argv[2];
+
+    {
+        // BAD: a string from the user is injected directly into
+        // a command line.
+        char command1[1000] = {0};
+        sprintf(command1, "userinfo -v \"%s\"", userName);
+        printf("Executing (insecure): %s\n", command1);
+        system(command1);
+    }
+
+    {
+        // GOOD: the user string is encoded by a library routine.
+        char userNameQuoted[1000] = {0};
+        encodeShellString(userNameQuoted, sizeof(userNameQuoted), userName);
+        char command2[1000] = {0};
+        sprintf(command2, "userinfo -v %s", userNameQuoted);
+        printf("Executing (secure): %s\n", command2);
+        system(command2);
+    }
+
+    return 0;
+}
+
