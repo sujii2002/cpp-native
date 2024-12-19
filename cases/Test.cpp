4bc2c7d7547f2b9a1f50dc7c5406122b32aa606c
sujii2002 <88040463+sujii2002@users.noreply.github.com>
2024-12-19 16:39:51 +0530

    Update Test.cpp

--- cases/Test.cpp
+++ cases/Test.cpp
@@ -85,7 +85,7 @@ int main(int argc, char **argv) {
     char *userAndFile = argv[2];
   
   {
-    char fileBuffer[PATH_MAX];
+    char fileBuffer[1000];
     snprintf(fileBuffer, sizeof(fileBuffer), "/home/%s", userAndFile);
     // BAD: a string from the user is used in a filename
     fopen(fileBuffer, "wb+");
